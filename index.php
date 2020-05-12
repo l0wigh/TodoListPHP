345e5c55810939d56f6c32e4c3c49ace35c7e42d
Thomas Mathiot <mathiotthomas@gmail.com>
2020-05-12 12:28:03 +0200

    Version fonctionnelle (pas forcément sans bugs dû aux manques de vérification des entrées utilisateur)

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+    if(isset($_GET["tache"])){
+        $fichier = fopen("list.csv", "a");
+        $tache = array($_GET["tache"]);
+        fputcsv($fichier, $tache);
+        fclose($fichier);
+    }
+    if(isset($_GET["lignedel"])){
+        $ligne = $_GET["lignedel"] - 1;
+        $csv = array_map('str_getcsv', file("list.csv"));
+        unset($csv[$ligne]);
+        $final = array_values($csv);
+        $fichier = fopen("list.csv", "w");
+        fputs($fichier, "");
+        fclose($fichier);
+        $fichier = fopen("list.csv", "a");
+        for($i = 0; $i < count($final); $i++){
+            $tache = array($final[$i][0]);
+            fputcsv($fichier, $tache);
+        }
+        fclose($fichier);
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TodoList</title>
+    </head>
+    <body>
+        <table border=1 style="padding: 1px; font-size: 40px;">
+            <?php
+                $fichier = fopen("list.csv", "r");
+                while($maligne = fgetcsv($fichier)) {
+                    echo '<tr>';
+                    foreach($maligne as $unecell){
+                        echo '<td>'.$unecell.'</td>';
+                    }
+                echo '</tr>';
+                }
+            ?>
+        </table>
+        <form method="GET">
+            <input type="text" name="tache" id="tache">
+            <button type="submit">Ajouter</button>
+        </form>
+        <form method="GET">
+            <input type="number" name="lignedel" id="lignedel">
+            <button type="submit">Supprimer</button>
+        </form>
+    </body>
+</html>
